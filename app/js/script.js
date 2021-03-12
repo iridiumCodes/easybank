fd03bd07ab1dd7b4de39dfbac6676e339492f9ab
Iris <iridiumcodes@gmail.com>
2021-03-12 22:08:38 +0200

    adds visibility hidden to be able to select text, and animation for hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,16 @@
 const btnHamburger = document.getElementById('btnHamburger');
 const header = document.querySelector('header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function () {
   this.classList.toggle('open');
   header.classList.toggle('open');
+
+  if (header.classList.contains('open')) {
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  } else {
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  }
 });
