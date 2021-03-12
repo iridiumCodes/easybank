364c4deb963f12081dbec8e860ae604d362a4851
Iris <iridiumcodes@gmail.com>
2021-03-12 23:27:04 +0200

    adds fade in to menu + no scroll when it's open

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,24 @@
+const body = document.querySelector('body');
 const btnHamburger = document.getElementById('btnHamburger');
 const header = document.querySelector('header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function () {
   this.classList.toggle('open');
   header.classList.toggle('open');
 
   if (header.classList.contains('open')) {
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    body.classList.add('noscroll');
+    fadeElements.forEach(function (element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
   } else {
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    body.classList.remove('noscroll');
+    fadeElements.forEach(function (element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
   }
 });
